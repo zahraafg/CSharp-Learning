7d27bf5b34bc610041da1292c6e3ccba115db1ff
Zahra <161948959+zahraafg@users.noreply.github.com>
2026-09-21 21:09:30 +0400

    Create Program_01.cs to print greeting message

---
+++ 01-Introduction/Program_01.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyFirstProgram
+{
+    internal class Program_01
+    {
+        public static void Run()
+        {
+            Console.Write("Hello, Zahra!");
+
+            Console.WriteLine(" I'm Learning C#.");
+
+            Console.ReadKey();
+        }
+    }
+}
