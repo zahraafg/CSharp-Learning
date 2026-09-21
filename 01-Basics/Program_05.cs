0a44c42b62c03eecd2b8938438b7a35617ada211
Zahra <161948959+zahraafg@users.noreply.github.com>
2026-09-21 21:19:57 +0400

    Add Program_05.cs demonstrating const usage

---
+++ 01-Basics/Program_05.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyFirstProgram
+{
+    internal class Program_05
+    {
+        public static void Run()
+        {
+
+           // when used const cant change number
+
+           const double pi = 3.14159;
+
+          // pi = 420;
+
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+        }
+    }
+}
